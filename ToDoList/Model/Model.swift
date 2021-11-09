a16f0b7a00afc6610ad6ee5afa16267a854396c9
Nicolae Pistriuga <87763817+PistriugaNicolay@users.noreply.github.com>
2021-11-09 22:43:42 +0200

    Init commit

---
+++ ToDoList/Model/Model.swift
@@ -0,0 +1,44 @@
+//
+//  Model.swift
+//  ToDoList
+//
+//  Created by IMAC on 07.11.2021.
+//
+
+import Foundation
+
+var ToDoItems: [[String: Any]]{
+    set {
+        UserDefaults.standard.set(newValue, forKey: "ToDoDataKey")
+        UserDefaults.standard.synchronize()
+    }
+    
+    get {
+        if let array = UserDefaults.standard.array(forKey: "ToDoDataKey") as? [[String: Any]]{
+            return array
+        } else {
+            return []
+        }
+    }
+}
+
+func addItem(nameItem: String, isCompleted: Bool = false) {
+    ToDoItems.append(["Name":nameItem, "isCompleted": false])
+}
+
+func removeItem(at index: Int) {
+    ToDoItems.remove(at: index)
+}
+
+func changeState(at item: Int) -> Bool {
+    ToDoItems[item]["isCompleted"] = !(ToDoItems[item]["isCompleted"] as! Bool)
+    
+    return ToDoItems[item]["isCompleted"] as! Bool
+}
+
+func moveItems(fromIndex: Int, toIndex: Int) {
+    let from = ToDoItems[fromIndex]
+    ToDoItems.remove(at: fromIndex)
+    ToDoItems.insert(from, at: toIndex)
+}
+
